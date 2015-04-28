07234c1791f70973701f9cc9f4f8dfa22d2e432f
Martin Höger <martin.hoger@gmail.com>
2015-04-28 16:05:39 +0200

    Comments removed.

--- custom/zaw-sources/wfile.zsh
+++ custom/zaw-sources/wfile.zsh
@@ -2,7 +2,6 @@ zmodload zsh/parameter
 
 function zaw-src-wfile() {
     candidates=($(cat ~/.cache/ctrlp/mru/cache.txt))
-    # candidates=($(cat ~/.vim-fuf-data/mrufile/items | grep -Po "/[^']+"))
     actions=("zaw-callback-wfile-vim" "zaw-callback-execute" "zaw-callback-append-to-buffer" "zaw-callback-replace-buffer")
     act_descriptions=("open in vim" "execute" "append to edit buffer" "replace edit buffer")
 }
